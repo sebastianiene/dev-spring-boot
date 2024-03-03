c383b74703c7c230584990a51b0f9f04a3a4bd26
seio86 <ene.sebastian@gmail.com>
2024-03-03 23:12:15 +0100

    added DemoController.java with a mapping for "/hello"; added endpoints "/showFormForAdd" and "/save" in EmployeeController and used a redirect to prevent duplicate submissions; in Employee.java made a constructor with parameters and overwrote toString(); in EmployeeService, save shall return void not an Employee; added the method findAllByOrderByLastNameAsc() in EmployeeRepository.java; added html forms

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
@@ -1,27 +1,28 @@
 package com.luv2code.springboot.thymeleafdemo.service;
 
-import com.luv2code.springboot.thymeleafdemo.dao.EmployeeRepository;
-import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import com.luv2code.springboot.thymeleafdemo.dao.EmployeeRepository;
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private EmployeeRepository employeeRepository;
-
-    @Autowired
-    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
-        employeeRepository = theEmployeeRepository;
-    }
-
-    @Override
-    public List<Employee> findAll() {
-        return employeeRepository.findAll();
-    }
+	private EmployeeRepository employeeRepository;
+	
+	@Autowired
+	public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+		employeeRepository = theEmployeeRepository;
+	}
+	
+	@Override
+	public List<Employee> findAll() {
+		return employeeRepository.findAllByOrderByLastNameAsc();
+	}
 
     @Override
     public Employee findById(int theId) {
@@ -40,10 +41,10 @@ public class EmployeeServiceImpl implements EmployeeService {
         return theEmployee;
     }
 
-    @Override
-    public Employee save(Employee theEmployee) {
-        return employeeRepository.save(theEmployee);
-    }
+	@Override
+	public void save(Employee theEmployee) {
+		employeeRepository.save(theEmployee);
+	}
 
     @Override
     public void deleteById(int theId) {
