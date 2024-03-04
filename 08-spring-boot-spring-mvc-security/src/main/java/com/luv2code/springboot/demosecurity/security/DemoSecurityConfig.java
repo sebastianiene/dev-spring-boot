d5dba06075a91b553751c915f9f319ed69256f2c
seio86 <ene.sebastian@gmail.com>
2024-03-04 22:06:51 +0100

    in DemoSecurityConfig.java permitting all to log out by writing .logout(logout -> logout.permitAll(); added a logout button to home.html page; in fancy-login.html added a You have been logged out message if="${param.logout}" is true for thymeleaf

--- 08-spring-boot-spring-mvc-security/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -47,6 +47,8 @@ public class DemoSecurityConfig {
                                 .loginPage("/showMyLoginPage")
                                 .loginProcessingUrl("/authenticateTheUser")
                                 .permitAll()
+                )
+                .logout(logout -> logout.permitAll()
                 );
 
         return http.build();
