e2be3c945beaa04cf702e2db678269a0c7837332
seio86 <ene.sebastian@gmail.com>
2024-03-05 22:07:18 +0100

    added findCoursesByInstructorId(theId) in which I created and executed a query: // create query         TypedQuery<Course> query = entityManager.createQuery(                                     "from Course where instructor.id = :data", Course.class);         query.setParameter("data", theId);
            // execute query
            List<Course> courses = query.getResultList();
    for the  @OneToMany annotation, used Lazy instead of Eager;
    created and called method findCoursesForInstructor(appDAO) inside CruddemoApplication.java;

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/dao/AppDAO.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/dao/AppDAO.java
@@ -1,7 +1,11 @@
 package com.luv2code.cruddemo.dao;
 
+import com.luv2code.cruddemo.entity.Course;
 import com.luv2code.cruddemo.entity.Instructor;
 import com.luv2code.cruddemo.entity.InstructorDetail;
+
+import java.util.List;
+
 public interface AppDAO {
 
     void save(Instructor theInstructor);
@@ -14,4 +18,5 @@ public interface AppDAO {
 
     void deleteInstructorDetailById(int theId);
 
+    List<Course> findCoursesByInstructorId(int theId);
 }
--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -1,11 +1,15 @@
 package com.luv2code.cruddemo.dao;
 
+import com.luv2code.cruddemo.entity.Course;
 import com.luv2code.cruddemo.entity.Instructor;
 import com.luv2code.cruddemo.entity.InstructorDetail;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class AppDAOImpl implements AppDAO {
@@ -60,5 +64,19 @@ public class AppDAOImpl implements AppDAO {
 
         // delete the instructor detail
         entityManager.remove(tempInstructorDetail);
+    }
+
+    @Override
+    public List<Course> findCoursesByInstructorId(int theId) {
+
+        // create query
+        TypedQuery<Course> query = entityManager.createQuery(
+                                    "from Course where instructor.id = :data", Course.class);
+        query.setParameter("data", theId);
+
+        // execute query
+        List<Course> courses = query.getResultList();
+
+        return courses;
     }
 }
