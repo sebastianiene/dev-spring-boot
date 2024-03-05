34777f9c4f07ffe254f3b4ff6ec9b0def6c1827b
seio86 <ene.sebastian@gmail.com>
2024-03-05 22:46:06 +0100

    in AppDAO added update(tempCourse) and findCourseById(theId); defined and called updateCourse(appDAO) method in CruddemoApplication;

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -31,10 +31,28 @@ public class CruddemoApplication {
 			// findInstructorWithCourses(appDAO);
 			// findCoursesForInstructor(appDAO);
 			// findInstructorWithCoursesJoinFetch(appDAO);
+			// updateInstructor(appDAO);
 
-			updateInstructor(appDAO);
+			updateCourse(appDAO);
 
 		};
+	}
+
+	private void updateCourse(AppDAO appDAO) {
+
+		int theId = 10;
+
+		// find the course
+		System.out.println("Finding course id: " + theId);
+		Course tempCourse = appDAO.findCourseById(theId);
+
+		// update the course
+		System.out.println("Updating course id: " + theId);
+		tempCourse.setTitle("Enjoy the Simple Things");
+
+		appDAO.update(tempCourse);
+
+		System.out.println("Done!");
 	}
 
 	private void updateInstructor(AppDAO appDAO) {
