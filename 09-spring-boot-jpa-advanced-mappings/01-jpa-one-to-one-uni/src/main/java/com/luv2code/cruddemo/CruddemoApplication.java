f37658d4c0208c71569c7b9c54e76aa35ebce00d
seio86 <ene.sebastian@gmail.com>
2024-03-06 22:52:44 +0100

    in Course class, added private List<Student> students field with two annotations @ManyToMany and @JoinTable, its getter and setter and also added a convenience method addStudent(theStudent); added class Student.java; in application.properties made the update spring.datasource.url=jdbc:mysql://localhost:3306/hb-05-many-to-many

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,10 +1,7 @@
 package com.luv2code.cruddemo;
 
 import com.luv2code.cruddemo.dao.AppDAO;
-import com.luv2code.cruddemo.entity.Course;
-import com.luv2code.cruddemo.entity.Instructor;
-import com.luv2code.cruddemo.entity.InstructorDetail;
-import com.luv2code.cruddemo.entity.Review;
+import com.luv2code.cruddemo.entity.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -38,9 +35,32 @@ public class CruddemoApplication {
 			// deleteCourse(appDAO);
 			// createCourseAndReviews(appDAO);
 			// retrieveCourseAndReviews(appDAO);
+			// deleteCourseAndReviews(appDAO);
 			
-			deleteCourseAndReviews(appDAO);
+			createCourseAndStudents(appDAO);
 		};
+	}
+
+private void createCourseAndStudents(AppDAO appDAO) {
+
+		// create a course
+		Course tempCourse = new Course("Pacman - How To Score One Million Points");
+
+		// create the students
+		Student tempStudent1 = new Student("John", "Doe", "john@luv2code.com");
+		Student tempStudent2 = new Student("Mary", "Public", "mary@luv2code.com");
+
+		// add students to the course
+		tempCourse.addStudent(tempStudent1);
+		tempCourse.addStudent(tempStudent2);
+
+		// save the course and associated students
+		System.out.println("Saving the course: " + tempCourse);
+		System.out.println("associated students: " + tempCourse.getStudents());
+
+		appDAO.save(tempCourse);
+
+		System.out.println("Done!");
 	}
 
 	private void deleteCourseAndReviews(AppDAO appDAO) {
@@ -65,6 +85,8 @@ public class CruddemoApplication {
 
 		// print the reviews
 		System.out.println(tempCourse.getReviews());
+
+		System.out.println("Done!");
 	}
 
 	private void createCourseAndReviews(AppDAO appDAO) {
--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/entity/Course.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -36,6 +36,17 @@ public class Course {
     @JoinColumn(name = "course_id")
     private List<Review> reviews;
 
+
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinTable(
+            name = "course_student",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private List<Student> students;
+
     public Course() {
 
     }
@@ -85,6 +96,25 @@ public class Course {
         }
 
         reviews.add(theReview);
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    // add a convenience method
+
+    public void addStudent(Student theStudent) {
+
+        if (students == null) {
+            students = new ArrayList<>();
+        }
+
+        students.add(theStudent);
     }
 
     @Override
