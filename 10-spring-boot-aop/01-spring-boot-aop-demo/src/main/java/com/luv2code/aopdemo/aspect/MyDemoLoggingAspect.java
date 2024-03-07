2b7a49fa9f166ebfab9007131d0f4db1ccf4ac31
seio86 <ene.sebastian@gmail.com>
2024-03-07 22:06:37 +0100

    in MyDemoLoggingAspect.java added argument to beforeAddAccountAdvice(JoinPoint theJoinPoint) and in this method,  displayed the method signature, called getArgs method, looped through the args to downcast and print Account specific stuff

--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,7 +1,10 @@
 package com.luv2code.aopdemo.aspect;
 
+import com.luv2code.aopdemo.Account;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -43,15 +46,33 @@ public class MyDemoLoggingAspect {
 	
 */
 	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
 
         System.out.println("\n=====>>> Executing @Before advice on method");
 
+        // display the method signature
+        MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+
+        System.out.println("Method: " + methodSignature);
+
+        // display method arguments
+
+        // get args
+        Object[] args = theJoinPoint.getArgs();
+
+        // loop thru args
+        for (Object tempArg : args) {
+            System.out.println(tempArg);
+
+            if (tempArg instanceof Account) {
+
+                // downcast and print Account specific stuff
+                Account theAccount = (Account) tempArg;
+
+                System.out.println("account name: " + theAccount.getName());
+                System.out.println("account level: " + theAccount.getLevel());
+            }
+        }
     }
 
-  /*  @Before("forDaoPackageNoGetterSetter()")
-    public void performApiAnalytics() {
-        System.out.println("\n=====>>> Performing API analytics");
-    }
-	*/
 }
