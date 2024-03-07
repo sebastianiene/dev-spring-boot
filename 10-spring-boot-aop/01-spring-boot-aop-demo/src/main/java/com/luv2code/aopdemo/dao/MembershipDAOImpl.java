d5f9a1896f78c13aa3f6b1b2952183044b68d123
seio86 <ene.sebastian@gmail.com>
2024-03-07 21:33:20 +0100

    in AccountDAO added @Before("execution(* add*(..))"); in AccountDAO added addAccount(theAccount, vipFlag); in AopdemoApplication, added theAccountDAO.doWork() and theMembershipDAO.goToSleep();

--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/dao/MembershipDAOImpl.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/dao/MembershipDAOImpl.java
@@ -12,4 +12,11 @@ public class MembershipDAOImpl implements MembershipDAO {
 
         return true;
     }
+
+    @Override
+    public void goToSleep() {
+
+        System.out.println(getClass() + ": I'm going to sleep now...");
+
+    }
 }
