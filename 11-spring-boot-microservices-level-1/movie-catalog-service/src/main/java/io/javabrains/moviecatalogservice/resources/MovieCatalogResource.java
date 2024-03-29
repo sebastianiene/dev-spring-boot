fc7bfd0d4a75d974ea6ca421b456e304cae617f2
seio86 <ene.sebastian@gmail.com>
2024-03-29 12:26:35 +0200

    imported and used webClientBuilder instead of the restTemplate

--- 11-spring-boot-microservices-level-1/movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ 11-spring-boot-microservices-level-1/movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -19,10 +20,14 @@ import java.util.stream.Collectors;
 public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
        // RestTemplate restTemplate = new RestTemplate();
+
+
 
         // get all rated movie IDs
         List<Rating> ratings = Arrays.asList(
@@ -31,7 +36,17 @@ public class MovieCatalogResource {
         );
 
         return ratings.stream().map(rating -> {
-                Movie movie= restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+        // this line below is deprecated
+           //     Movie movie= restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+
+             Movie movie = webClientBuilder.build()
+                     .get()
+                     .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                     .retrieve()  //do the fetch
+                     .bodyToMono(Movie.class) //whatever body you get, convert it to Movie.class, where Mono is a reactive way of promise that you will get it asynchronous eventually
+                     .block(); //blocking execution until Mono is fulfilled
+
+
                 return new CatalogItem(movie.getName(), "Description", rating.getRating());
         })
         .collect(Collectors.toList());
