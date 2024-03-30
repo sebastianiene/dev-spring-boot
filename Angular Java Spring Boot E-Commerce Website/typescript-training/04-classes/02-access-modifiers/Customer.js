94852aa944ffb3e551f0099e2c75da60c7de010c
seio86 <ene.sebastian@gmail.com>
2024-03-30 21:05:13 +0200

    typescript crash course: fundamentals, creating classes, inheritance and interfaces

---
+++ Angular Java Spring Boot E-Commerce Website/typescript-training/04-classes/02-access-modifiers/Customer.js
@@ -0,0 +1,35 @@
+var Customer = /** @class */ (function () {
+    function Customer(theFirst, theLast) {
+        this._firstName = theFirst;
+        this._lastName = theLast;
+    }
+    Object.defineProperty(Customer.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            this._firstName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Customer.prototype, "lastName", {
+        get: function () {
+            return this._lastName;
+        },
+        set: function (value) {
+            this._lastName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Customer;
+}());
+// let's create an instance
+var myCustomer = new Customer("Martin", "Dixon");
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
+myCustomer.firstName = "Susan";
+myCustomer.lastName = "Public";
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
