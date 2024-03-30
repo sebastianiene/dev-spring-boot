f4ed1fe248bda968e490538c335a5c495b0afb52
seio86 <ene.sebastian@gmail.com>
2024-03-30 22:26:42 +0200

    angular crash course: new component, bootstrap CSS, conditionals and formatting (USD currency addition)

---
+++ Angular Java Spring Boot E-Commerce Website/angular-training/my-first-angular-project/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'This is My First App! YES YES SUCCESS!';
+
+  firstName='Bill';
+  lastName='Smith';
+
+}
