c9fc0aa3a1017eb8b1f38610a507675df2cc5b89
seio86 <ene.sebastian@gmail.com>
2024-04-01 22:38:34 +0300

    secured API in OKTA website and added openssl certificates, from now on Angular starts with "npm start"

--- Angular Java Spring Boot E-Commerce Website/ecommerce-project/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ Angular Java Spring Boot E-Commerce Website/ecommerce-project/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oag5gvfzeOjkFJyy5d7',
         issuer: 'https://dev-66193346.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 
