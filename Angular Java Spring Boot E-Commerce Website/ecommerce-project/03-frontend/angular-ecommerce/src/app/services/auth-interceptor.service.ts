dede512f6cf47b6d0ac66b150b3426490bd0a4cd
seio86 <ene.sebastian@gmail.com>
2024-04-01 23:17:29 +0300

    configured backend to run on a QA demo port 9898 and frontend to use a environment.qa.ts file for QA sessions

--- Angular Java Spring Boot E-Commerce Website/ecommerce-project/03-frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
+++ Angular Java Spring Boot E-Commerce Website/ecommerce-project/03-frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
@@ -19,7 +19,8 @@ export class AuthInterceptorService implements HttpInterceptor {
   private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
 
     // Only add an access token for secured endpoints
-    const securedEndpoints = ['http://localhost:8080/api/orders'];
+    const theEndpoint = environment.luv2shopApiUrl + '/orders';
+    const securedEndpoints = [theEndpoint];
 
     if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
 
